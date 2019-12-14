4fe1eeeeaaa0727d2a769099d4a1769f21c092c7
blitzparanoia <bluemilk44@gmail.com>
2019-12-13 19:14:41 -0500

    created reducer files and set up reducer

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import { Provider } from  'react-redux'
-
+import bookReducer from './reducers/bookReducer'
 
 import App from './App';
 
 //allows for the dev tools to be used
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-let store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
+let store = createStore(bookReducer, composeEnhancers(applyMiddleware(thunk)))
 
 
 ReactDOM.render(
