71eef458f17968f541c957578d11125a0a85c0d4
blitzparanoia <bluemilk44@gmail.com>
2019-12-15 21:12:34 -0500

    imported browser router and wrapped the App in router

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import { Provider } from  'react-redux'
+import { BrowserRouter as Router } from 'react-router-dom'
+
 import bookReducer from './reducers/bookReducer'
 
 import App from './App';
@@ -15,7 +17,9 @@ let store = createStore(bookReducer, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
 <Provider store={store}>
+    <Router>
     <App />
+    </Router>
 </Provider>
     ,
      document.getElementById('root'));
