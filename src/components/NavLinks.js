561ce1f92f5bad71221be698ad3ba4a89a5d10fd
blitzparanoia <bluemilk44@gmail.com>
2019-12-22 14:08:48 -0500

    added links for viewing all books and adding a book, links work

--- src/components/NavLinks.js
+++ src/components/NavLinks.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const NavLinks = (props) => {
     return(
         <div className="NavLinks">
-            Links
+            <Link to="/books" style={{ paddingRight: '10px' }}>Books</Link>
+
+            <Link to="/books/new">Add Book</Link>
 
         </div>
     )
